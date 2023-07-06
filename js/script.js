7f216d636af6dc677a94911c19ac9ba7861832a1
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-07-06 15:27:26 +0800

    Create operation functions

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Create operator functions.
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+console.log(add(1, 1))
+console.log(subtract(2, 1))
+console.log(multiply(2, 3))
+console.log(divide(4, 2))
